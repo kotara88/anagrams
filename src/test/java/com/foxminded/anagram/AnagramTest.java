5b6487ecdc0dc79abd1e7f215eb9a763fb5ba0e9
mikhail <mkotara8827@gmail.com>
2018-07-14 15:50:55 +0300

    test data was simplified

--- src/test/java/com/foxminded/anagram/AnagramTest.java
+++ src/test/java/com/foxminded/anagram/AnagramTest.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnagramTest {
+    private String sentence = "1abcd ab1cd abcd1";
+
     @Test
     public void sentenceShouldBeReversed() {
         Anagram anagram = new Anagram();
-        String sentence = "1aB<cd!# ^Efg?h)7";
-        String expected = "1dc<Ba!# ^hgf?E)7";
+        String expected = "1dcba dc1ba dcba1";
         String result = anagram.reversSentence(sentence);
         Assert.assertEquals(expected,result);
     }
